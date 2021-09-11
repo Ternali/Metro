c2c212634be0d4284819940be59a05b9fd6c527b
pekkasuper <3062055813@qq.com>
2021-09-11 23:48:21 +0800

    建立三合一树先前测试

--- 031902309/pinyintest.py
+++ 031902309/pinyintest.py
@@ -12,15 +12,20 @@ print(str(tmp[0][0]))
 p = Pinyin()
 result = p.get_pinyin('真的假的')
 print(result)
-#替换无用的-用来提取拼音
-result = re.sub('-', '', result)
+# 替换无用的-用来提取拼音
+tmp_result = re.sub('-', '', result)
+print(tmp_result)
+# 用来切割每个汉字对应的读音
+result = result.split('-')
 print(result)
 
 # 用以测试提取拼音首字母
 result = p.get_initials("真的假的")
 print(result)
 # 替换无用的-用于提取拼音首字母
-result = re.sub('-', '', result)
+tmp_result = re.sub('-', '', result)
+print(tmp_result)
+# 用于切割每个汉字拼音首字母
+result = result.split('-')
 print(result)
-# 还是xpinyin香，用太多会拉胯还是不转换成大写
 
