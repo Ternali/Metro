55e1d6ad2eb876ebbf41b8bb94ff79d0141b8317
pekkasuper <3062055813@qq.com>
2021-09-11 19:47:49 +0800

    xpinyin真香

--- 031902309/pinyintest.py
+++ 031902309/pinyintest.py
@@ -16,3 +16,11 @@ print(result)
 result = re.sub('-', '', result)
 print(result)
 
+# 用以测试提取拼音首字母
+result = p.get_initials("真的假的")
+print(result)
+# 替换无用的-用于提取拼音首字母
+result = re.sub('-', '', result)
+print(result)
+# 还是xpinyin香，用太多会拉胯还是不转换成大写
+
