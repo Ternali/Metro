40bb28768edde759a54c7ffe9143a150dda2b0a4
pekkasuper <3062055813@qq.com>
2021-09-13 12:57:12 +0800

    汉字四角编码

---
+++ 031902309/four_corner/__init__.py
@@ -0,0 +1,14 @@
+import pickle
+import pkg_resources
+
+
+class FourConrner:
+    # 读取已有汉字四角编码
+    def __init__(self):
+        data_file = pkg_resources.resource_filename(__name__, "data/data.pkl")
+        f = open(data_file, 'rb')
+        self.data = pickle.load(f)      
+    # 获取对应汉字的四角编码
+    def query(self, hanzi):
+        return self.data.get(hanzi)
+
