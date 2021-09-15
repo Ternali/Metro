ede9493d46159ac4de7710c4d3f94c8e6bbb9d3c
pekkasuper <3062055813@qq.com>
2021-09-16 00:52:56 +0800

    入口修改

--- 031902309/main.py
+++ 031902309/main.py
@@ -2,20 +2,20 @@ import sys
 
 from ACTrie.actrie import Trie
 
-# args = sys.argv
+args = sys.argv
 AcTrie = Trie()
-f = open("words.txt", encoding="utf-8")
+f = open(args[1], encoding="utf-8")
 # 读取敏感词文件一次性读取所有行数
 word_store = f.readlines()
 f.close()
 # 开始插入敏感词到树中
 AcTrie.prepareWork(word_store)
 # 读入待检测文件
-f = open("org.txt", encoding="utf-8")
+f = open(args[2], encoding="utf-8")
 contents = f.readlines()
 f.close()
 # 开始匹配对应的字段
 for line, content in enumerate(contents):
     AcTrie.search(content, line + 1)
 # 写入到文件中
-AcTrie.writeFile("ans.txt")
+AcTrie.writeFile(args[3])
